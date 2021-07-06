895a2af9215279efb75591f668231e84282e99f1
Tangjj1996 <tangjj_1996@163.com>
2021-07-06 23:36:26 +0800

    fix: npm publish script complete

--- deploy/action.sh
+++ deploy/action.sh
@@ -7,7 +7,7 @@ yarn build
 VERSION=`node deploy/select-cli.js`
 
 git add -A
-npm version --force $VERSION -m "feat: [release] v.$VERSION success"
+npm version --force $VERSION -m "feat: [release] v$VERSION success"
 npm publish
 
-echo -e "\033[32m 🎁 congratulation~ you have released: \0333[1mv.$VERSION\033[0m"
+echo -e "\033[32m 🎁 congratulation~ you have released: \033[1mv$VERSION\033[0m"
