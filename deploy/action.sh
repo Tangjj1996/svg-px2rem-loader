17e88a44580f69dad86159dfa443aa3ef10d8f1f
Tangjj1996 <tangjj_1996@163.com>
2021-07-06 17:44:07 +0800

    fix: deploy script

--- deploy/action.sh
+++ deploy/action.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
 
+yarn build
+
 VERSION=`node deploy/select-cli.js`
 
-echo "release $VERSION"
 
 npm version $VERSION -m "[release] $VERSION"
 npm publish
+echo -e "\033[32mrelease: $VERSION\033[0m"
