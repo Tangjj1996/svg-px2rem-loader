044581756b82c4121d7971a8784e5127a258a9da
Tangjj1996 <tangjj_1996@163.com>
2021-07-30 11:40:24 +0800

    chores: .editorconfig

--- test/compiler_as_preloader.js
+++ test/compiler_as_preloader.js
@@ -3,47 +3,47 @@ import webpack from 'webpack'
 import { createFsFromVolume, Volume } from 'memfs'
 
 export default (fixture, options = {}) => {
-  const compiler = webpack({
-    context: __dirname,
-    entry: `./${fixture}`,
-    output: {
-      path: path.resolve(__dirname),
-      filename: 'bundle.js',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.svg$/,
-          use: [
-            {
-              loader: 'svg-inline-loader',
-              options: {
-                removeSVGTagAttrs: false,
-              },
-            },
-            {
-              loader: path.resolve(__dirname, '../dist/index.js'),
-              options: {
-                rootValue: 32,
-              },
-            },
-          ],
-        },
-      ],
-    },
-  })
+	const compiler = webpack({
+		context: __dirname,
+		entry: `./${fixture}`,
+		output: {
+			path: path.resolve(__dirname),
+			filename: 'bundle.js',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.svg$/,
+					use: [
+						{
+							loader: 'svg-inline-loader',
+							options: {
+								removeSVGTagAttrs: false,
+							},
+						},
+						{
+							loader: path.resolve(__dirname, '../dist/index.js'),
+							options: {
+								rootValue: 32,
+							},
+						},
+					],
+				},
+			],
+		},
+	})
 
-  compiler.outputFileSystem = createFsFromVolume(new Volume())
-  compiler.outputFileSystem.join = path.join.bind(path)
+	compiler.outputFileSystem = createFsFromVolume(new Volume())
+	compiler.outputFileSystem.join = path.join.bind(path)
 
-  return new Promise((resolve, reject) => {
-    compiler.run((err, stats) => {
-      if (err) reject(err)
-      if (stats.hasErrors()) {
-        reject(stats.toJson().errors)
-      }
+	return new Promise((resolve, reject) => {
+		compiler.run((err, stats) => {
+			if (err) reject(err)
+			if (stats.hasErrors()) {
+				reject(stats.toJson().errors)
+			}
 
-      resolve(stats)
-    })
-  })
+			resolve(stats)
+		})
+	})
 }
