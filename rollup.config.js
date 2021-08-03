c22c008ce00dff5563ff45b946949960cf882a5d
Tangjj1996 <tangjj_1996@163.com>
2021-08-03 14:41:50 +0800

    chores: add editorconfig

--- rollup.config.js
+++ rollup.config.js
@@ -4,17 +4,17 @@ import rollupCommonjs from '@rollup/plugin-commonjs'
 import rollupJson from '@rollup/plugin-json'
 
 export default {
-	input: resolve(__dirname, 'src/index.js'),
-	output: [
-		{
-			file: 'dist/cjs.js',
-			format: 'cjs',
-			exports: 'default', // you should set out.export to 'auto' or 'default' while you are using es-module export default syntax
-		},
-		{
-			file: 'dist/index.js',
-			format: 'esm',
-		},
-	],
-	plugins: [rollupResolve(), rollupCommonjs(), rollupJson()],
+  input: resolve(__dirname, 'src/index.js'),
+  output: [
+    {
+      file: 'dist/cjs.js',
+      format: 'cjs',
+      exports: 'default', // you should set out.export to 'auto' or 'default' while you are using es-module export default syntax
+    },
+    {
+      file: 'dist/index.js',
+      format: 'esm',
+    },
+  ],
+  plugins: [rollupResolve(), rollupCommonjs(), rollupJson()],
 }
--- test/loader.test.js
+++ test/loader.test.js
@@ -6,15 +6,15 @@ import compilerAsPreloader from './compiler_as_preloader'
 import compilerAsLastloader from './compiler_as_lastloader'
 
 test('Inserts name add outputs javascript', async () => {
-	const stats = await compilerAsPreloader('example.svg')
-	const output = stats.toJson({ source: true }).modules[0].source
+    const stats = await compilerAsPreloader('example.svg')
+    const output = stats.toJson({ source: true }).modules[0].source
 
-	expect(output).toMatch('rem')
+    expect(output).toMatch('rem')
 })
 
 test('Inserts name add outputs javascript when is the lastest loader', async () => {
-	const stats = await compilerAsLastloader('example.svg')
-	const output = stats.toJson({ source: true }).modules[0].source
+    const stats = await compilerAsLastloader('example.svg')
+    const output = stats.toJson({ source: true }).modules[0].source
 
-	expect(output).toMatch('svg')
+    expect(output).toMatch('svg')
 })
